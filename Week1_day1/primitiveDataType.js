7f0bf4fe5cbb897c7023238799fee9fdc879160e
LavanyaThiyagarajan <141829999+LavanyaThiyagarajan@users.noreply.github.com>
2026-07-19 09:09:58 +0530

    Add files via upload
    week1day1

---
+++ Week1_day1/primitiveDataType.js
@@ -0,0 +1,18 @@
+let firstName = "Lavanya";
+let companyName = "Capgemini";
+let mobileNumber= 1234567890;
+let isAutomation = true;
+let hasPlaywright;
+
+console.log("Values of the variables are: ");
+console.log(firstName);
+console.log(companyName);
+console.log(mobileNumber);
+console.log(isAutomation);
+console.log(hasPlaywright);
+console.log("Data Types of the variables are: ");
+console.log(typeof firstName);
+console.log(typeof companyName);
+console.log(typeof mobileNumber);
+console.log(typeof isAutomation);
+console.log(typeof hasPlaywright);
